f2080ff434d27e4306d42083d03799103f108a75
jaideep ballani <jaideepballani1@gmail.com>
2021-03-24 00:37:44 +0800

    streamlit app2

--- app.py
+++ app.py
@@ -1,64 +1,46 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
+from datetime import datetime
+import numpy as np
+import plotly.express as px
 
-import matplotlib.pyplot as plt
-import seaborn as sns
+st.title("NAS Daily Course Dashboard")
+#st.header("Data Visualization Board for Winery Inc Chemical Components")
+df = pd.read_csv("./Customer Info_NasAcademy.csv")
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+n_tz = df['timezone'].nunique()
+top_k_tz = st.sidebar.slider('Choose the Number of Top Earning Timezones', 0, n_tz+1, (0, n_tz+1), 1)[1]
+st.subheader("Most Profitable Timezones")
+tmp = df.groupby('timezone')['fullAmount'].apply(sum)
+by_timezone = pd.DataFrame({'timezone':tmp.index, 'totalAmount':tmp}).sort_values(['totalAmount'], ascending = False).reset_index(drop=True)
+st.dataframe(by_timezone.head(top_k_tz))
 
+n_course = df['courseOfferId'].nunique()
+top_k_courses = st.sidebar.slider('Choose the Number of Top Earning Courses', 0, n_course+1, (0, n_course+1), 1)[1]
+#top_k_courses = n_course if top_k_courses > n_course else top_k_courses
+st.subheader("Most Profitable Courses")
+tmp = df.groupby('courseOfferId')['fullAmount'].apply(sum)
+by_timezone = pd.DataFrame({'courseOfferId':tmp.index, 'totalAmount':tmp}).sort_values(['totalAmount'], ascending = False).reset_index(drop=True)
+st.dataframe(by_timezone.head(top_k_courses))
 
-def main():
+# Tenure
+current_ts = datetime.today().strftime('%d-%m-%Y %H:%M')
+n_students = df['id'].nunique()
+top_k_tenure = st.sidebar.slider('Choose the Number of Students to Visualise Lifecycles', 0, n_students, (0, n_students), 1)[1]
+st.subheader("Top Students by Lifecycle")
+timedelta = df.apply(lambda row : pd.to_datetime(df['createTime'].max(), format='%d-%m-%Y %H:%M') - pd.to_datetime(row['createTime'], format='%d-%m-%Y %H:%M'), axis=1)
+df['days'] = timedelta.dt.days
+df['hours'] = timedelta.dt.components.hours
+df['minutes'] = timedelta.dt.components.minutes
+cols = ['id','firstName','lastName','days','hours','minutes']
+st.dataframe(df[cols].head(top_k_tenure))
 
-    df = load_data()
+# st.subheader('Tenure Lifecycle')
+# hist_values = np.histogram(df['days'].head(top_k_tenure), bins=100, range=(0,100))[0]
+# st.bar_chart(hist_values)
 
-    page = st.sidebar.selectbox("Choose a page", ['Homepage', 'Exploration', 'Prediction'])
-
-    if page == 'Homepage':
-        st.title('Wine Alcohol Class Prediction')
-        st.text('Select a page in the sidebar')
-        st.dataframe(df)
-    elif page == 'Exploration':
-        st.title('Explore the Wine Data-set')
-        if st.checkbox('Show column descriptions'):
-            st.dataframe(df.describe())
-        
-        st.markdown('### Analysing column relations')
-        st.text('Correlations:')
-        fig, ax = plt.subplots(figsize=(10,10))
-        sns.heatmap(df.corr(), annot=True, ax=ax)
-        st.pyplot()
-        st.text('Effect of the different classes')
-        sns.pairplot(df, vars=['magnesium', 'flavanoids', 'nonflavanoid_phenols', 'proline'], hue='alcohol')
-        st.pyplot()
-    else:
-        st.title('Modelling')
-        model, accuracy = train_model(df)
-        st.write('Accuracy: ' + str(accuracy))
-        st.markdown('### Make prediction')
-        st.dataframe(df)
-        row_number = st.number_input('Select row', min_value=0, max_value=len(df)-1, value=0)
-        st.markdown('#### Predicted')
-        st.text(model.predict(df.drop(['alcohol'], axis=1).loc[row_number].values.reshape(1, -1))[0])
-
-
-@st.cache
-def train_model(df):
-    X = np.array(df.drop(['alcohol'], axis=1))
-    y= np.array(df['alcohol'])
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    model = RandomForestClassifier()
-    model.fit(X_train, y_train)
-
-    return model, model.score(X_test, y_test)
-
-@st.cache
-def load_data():
-    return pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', names=['alcohol', 'malic_acid', 'ash', 'alcalinity_of_ash', 'magnesium', 'total_phenols','flavanoids', 'nonflavanoid_phenols' ,'proanthocyanins', 'color_intensity', 'hue', 'OD280/OD315_of_diluted_wines', 'proline'], delimiter=",", index_col=False)
-
-
-if __name__ == '__main__':
-    main()
+#st.subheader('Tenure Lifecycle')
+f = px.histogram(df.head(top_k_tenure), x="days", nbins=100, title="Lifecycle distribution")
+f.update_xaxes(title="Lifecycle")
+f.update_yaxes(title="No. of students")
+st.plotly_chart(f)
